4a58a461b6a088f786d8bd457b547f3977867bf4
duandun <duandun@gmail.com>
2017-06-19 01:35:04 +0800

    update

--- build/upload.sh
+++ build/upload.sh
@@ -6,4 +6,4 @@ echo "压缩打包中..."
 mkdir -p output
 tar zcvf tangbang-front.tar.gz dist/index.html dist/static
 mv tangbang-front.tar.gz output
-scp -P 26303 output/tangbang-front.tar.gz root@104.224.187.168:/home/tangbang-front
+scp output/tangbang-front.tar.gz root@111.204.219.209:/home/tangbang-front
